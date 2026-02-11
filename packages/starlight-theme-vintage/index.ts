5e0334da0b1f416ea8438f7d3fed5d0d2aaf4124
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-02-11 15:53:30 +0100

    feat: layout + page

--- packages/starlight-theme-vintage/index.ts
+++ packages/starlight-theme-vintage/index.ts
@@ -7,44 +7,47 @@ export default function starlightThemeRapidePlugin(): StarlightPlugin {
     name: 'starlight-theme-rapide',
     hooks: {
       'config:setup'({ config, logger, updateConfig }) {
-        const userExpressiveCodeConfig =
-          !config.expressiveCode || config.expressiveCode === true ? {} : config.expressiveCode
+        // TODO(HiDeoo)
+        // const userExpressiveCodeConfig =
+        //   !config.expressiveCode || config.expressiveCode === true ? {} : config.expressiveCode
 
-        // updateConfig({
-        //   components: overrideComponents(config, ['LanguageSelect', 'Pagination', 'ThemeSelect'], logger),
-        //   customCss: [
-        //     ...(config.customCss ?? []),
-        //     'starlight-theme-rapide/styles/layers',
-        //     'starlight-theme-rapide/styles/theme',
-        //     'starlight-theme-rapide/styles/base',
-        //     ...(config.markdown?.headingLinks === false ? [] : ['starlight-theme-rapide/styles/anchors']),
-        //   ],
-        //   expressiveCode:
-        //     config.expressiveCode === false
-        //       ? false
-        //       : {
-        //           themes: ['vitesse-dark', 'vitesse-light'],
-        //           ...userExpressiveCodeConfig,
-        //           styleOverrides: {
-        //             borderColor: 'var(--sl-rapide-ui-border-color)',
-        //             borderRadius: '0.5rem',
-        //             ...userExpressiveCodeConfig.styleOverrides,
-        //             frames: {
-        //               editorActiveTabIndicatorTopColor: 'unset',
-        //               editorActiveTabIndicatorBottomColor: 'var(--sl-color-gray-3)',
-        //               editorTabBarBorderBottomColor: 'var(--sl-rapide-ui-border-color)',
-        //               frameBoxShadowCssValue: 'unset',
-        //               ...userExpressiveCodeConfig.styleOverrides?.frames,
-        //             },
-        //             textMarkers: {
-        //               backgroundOpacity: '40%',
-        //               markBackground: 'var(--sl-rapide-ec-marker-bg-color)',
-        //               markBorderColor: 'var(--sl-rapide-ec-marker-border-color)',
-        //               ...userExpressiveCodeConfig.styleOverrides?.textMarkers,
-        //             },
-        //           },
-        //         },
-        // })
+        updateConfig({
+          // components: overrideComponents(config, ['LanguageSelect', 'Pagination', 'ThemeSelect'], logger),
+          customCss: [
+            ...(config.customCss ?? []),
+            'starlight-theme-vintage/styles/layers',
+            'starlight-theme-vintage/styles/theme',
+            'starlight-theme-vintage/styles/base',
+            // TODO(HiDeoo)
+            // ...(config.markdown?.headingLinks === false ? [] : ['starlight-theme-vintage/styles/anchors']),
+          ],
+          // TODO(HiDeoo)
+          // expressiveCode:
+          //   config.expressiveCode === false
+          //     ? false
+          //     : {
+          //         themes: ['vitesse-dark', 'vitesse-light'],
+          //         ...userExpressiveCodeConfig,
+          //         styleOverrides: {
+          //           borderColor: 'var(--sl-rapide-ui-border-color)',
+          //           borderRadius: '0.5rem',
+          //           ...userExpressiveCodeConfig.styleOverrides,
+          //           frames: {
+          //             editorActiveTabIndicatorTopColor: 'unset',
+          //             editorActiveTabIndicatorBottomColor: 'var(--sl-color-gray-3)',
+          //             editorTabBarBorderBottomColor: 'var(--sl-rapide-ui-border-color)',
+          //             frameBoxShadowCssValue: 'unset',
+          //             ...userExpressiveCodeConfig.styleOverrides?.frames,
+          //           },
+          //           textMarkers: {
+          //             backgroundOpacity: '40%',
+          //             markBackground: 'var(--sl-rapide-ec-marker-bg-color)',
+          //             markBorderColor: 'var(--sl-rapide-ec-marker-border-color)',
+          //             ...userExpressiveCodeConfig.styleOverrides?.textMarkers,
+          //           },
+          //         },
+          //       },
+        })
       },
     },
   }
